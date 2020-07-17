8f2ddbe77b9eae8d9908c16ed4f9e7242966b1ae
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-07-17 06:02:45 -0300

    funcion logout agregada

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,34 +2,38 @@
 var express = require("express");
 var router = express.Router();
 const usersController = require("../controllers/usersController");
-const avatarController = require("../controllers/avatarController")
-const path = require('path')
+const avatarController = require("../controllers/avatarController");
+const path = require("path");
 let { check, validationResult, body } = require("express-validator");
 
-
-var multer = require('multer');
+var multer = require("multer");
 
 var storage = multer.diskStorage({
-  destination:(req,res,cb) => {
-    cb(null,'./public/images')
+  destination: (req, res, cb) => {
+    cb(null, "./public/images");
   },
-  filename:(req,file,cb) => {
-    let ext = path.extname(file.originalname)
-    cb(null, `${file.fieldname}-${Date.now()}-${ext}`)
-  }
-})
+  filename: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${Date.now()}-${ext}`);
+  },
+});
 
-var upload = multer({storage})
+var upload = multer({ storage });
 
-
-let registerValidation = require('../middlewares/registerValidation')
+let registerValidation = require("../middlewares/registerValidation");
 
 router.get("/login", usersController.login);
 
-router.post("/login",[
-  check('email').isEmail().withMessage('Email Invalido'),
-  check('password').isLength({min:8}).withMessage('La contrasenia debe tener al menos 8 caracteres')
-], usersController.processLogin);
+router.post(
+  "/login",
+  [
+    check("email").isEmail().withMessage("Email Invalido"),
+    check("password")
+      .isLength({ min: 8 })
+      .withMessage("La contrasenia debe tener al menos 8 caracteres"),
+  ],
+  usersController.processLogin
+);
 
 router.get("/register", usersController.register);
 
@@ -37,17 +41,18 @@ router.post("/register", usersController.createUser);
 
 //router.post("/register",upload.any(), usersController.store);
 
+router.get("/logout", function (req, res) {
+  req.session.destroy(() => {
+    res.redirect("/users/login");
+  });
+});
 
+router.get("/check", function (req, res) {
+  req.session.usuarioLog = "prueba@hotmail.com";
 
+  console.log(req.session);
 
-router.get('/check',function(req,res){
+  console.log(req.session.usuariolog);
+});
 
-  req.session.usuarioLog = "prueba@hotmail.com"
-
-  console.log(req.session)
-
-  console.log(req.session.usuariolog)
-
-})
-
-module.exports = router
+module.exports = router;
