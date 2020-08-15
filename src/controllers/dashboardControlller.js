a3633b413661c8975e370777f7a9bf17364d8e05
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-08-15 09:00:36 -0300

    Avanzando con el CRUD en los usuarios

--- src/controllers/dashboardControlller.js
+++ src/controllers/dashboardControlller.js
@@ -5,6 +5,76 @@ const db = require(dbDir);
 
 module.exports = {
 
+    //Users
+
+    showUsers:function (req, res) {
+      db.User.findAll()
+        .then((result) => {
+          res.render("dashboard/index", {
+            users: result,
+            title: "Proyecto",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json({ error: true });
+        });
+    },
+
+    storeUser: function (req, res) {
+      db.Product.create({
+        nombre: req.body.nombre,
+        precio: req.body.precio,
+      })
+      .then(() => {
+        return res.redirect("/dashboard/products");
+      });
+    },
+
+    editUser: function (req, res) {
+      let productReq = db.Product.findAll();
+      let categoryReq = db.Category.findAll();
+  
+      Promise.all([productReq, categoryReq])
+        .then(function ([products, category]) {
+          res.render("dashboard/products", {
+            products,
+            category,
+            title: "Proyecto",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  
+    updateUser: function (req, res) {
+      db.User.update(
+        {
+          nombre: req.body.nombre,
+          precio: req.body.precio,
+        },
+        {
+          where: {
+            idUsuario: req.params.id,
+          },
+        }
+      ).then(() => {
+        return res.redirect("/dashboard");
+      });
+    },
+  
+    deleteUser:function (req, res) {
+      db.User.destroy({
+        where: { idUsuario: req.params.id},
+      }).then(() => {
+        res.redirect("/dashboard/products");
+      });
+    },  
+  
+  
+
+  // Products
   show:function (req, res) {
     db.Product.findAll()
       .then((products) => {
@@ -71,21 +141,6 @@ module.exports = {
   },
 
   
-  //Users
-
-  showUsers:function (req, res) {
-    db.User.findAll()
-      .then((result) => {
-        res.render("dashboard/index", {
-          users: result,
-          title: "Proyecto",
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: true });
-      });
-  },
 
   logout: function (req, res) {
     req.session.destroy();
