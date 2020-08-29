80ede07d8319c7fe2f5d920ba87bc331e932faf3
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-08-29 06:17:10 -0300

    Intentando mostrar los productos desde Publications

---
+++ src/controllers/publicationController.js
@@ -0,0 +1,57 @@
+// DB
+const path = require("path");
+const { render } = require("ejs");
+dbDir = path.resolve("db", "models");
+const db = require(dbDir);
+
+module.exports = {
+  // PublicationView
+  show: function (req, res) {
+    // Trayendo Estados
+
+    db.Publication.findAll({
+      include: ["Producto"],
+      raw: true,
+      nest: true,
+    })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: true });
+      });
+  },
+
+  form: function (req, res) {
+    db.Category.findAll()
+      .then((result) => {
+        res.render("publication/form", {
+          categorias: result,
+          session: req.session,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: true });
+      });
+  },
+
+  store: function (req, res) {
+    db.Product.create({
+      nombre: req.body.nombre,
+      marca: req.body.marca,
+      modelo: req.body.modelo,
+      precio: req.body.precio,
+      stock: req.body.stock,
+      idCategoria: req.body.idCategoria,
+      img: path.join("img", req.file.filename),
+    }).then((product) => {
+      db.Publication.create({
+        idProducto: product.idProducto,
+      });
+    });
+
+    res.redirect("/");
+  },
+};
