2dedf04219f0bfdb9da84bf35dbe7d20a74d0c32
lucas cantero <lucas_2_4@hotmail.com>
2020-09-07 01:09:17 -0300

    se agrego la funcionalidad del search

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const path = require("path");
 dbDir = path.resolve("db", "models");
 const db = require(dbDir);
-
+const { Op } = require("sequelize");
 // ************ Controller Require ************
 const controllers = require("../controllers");
 const usuariosMiddlewares = require("../middlewares/usuariosMiddlewares");
@@ -14,131 +14,160 @@ const usuariosMiddlewares = require("../middlewares/usuariosMiddlewares");
 router.get("/", controllers.main.root); /* GET - home page */
 
 router.get(
-  "/checkout",
-  usuariosMiddlewares,
-  controllers.main.checkout
+	"/checkout",
+	usuariosMiddlewares,
+	controllers.main.checkout
 ); /* GET - checkout */
 
 // Probando las consultas
 
 router.get("/idUsuario", function (req, res) {
-  res.json(req.session.user.idUsuario);
+	res.json(req.session.user.idUsuario);
 });
 
 // Trayendo Producto
 router.get("/traerProducto", function (req, res) {
-  db.Product.findAll({
-    include: ["estadoProducto"],
-  })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Product.findAll({
+		include: ["estadoProducto"],
+	})
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo Producto
 router.get("/store", function (req, res) {
-  db.Publication.findAll({
-    include: ["product"],
-  })
-    .then((publications) => {
-      res.render("store", {
-        session: req.session,
-        publications,
-        cart: req.session.cart,
-        wishlist:req.session.wishlist
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Publication.findAll({
+		include: ["product"],
+	})
+		.then((publications) => {
+			res.render("store", {
+				session: req.session,
+				publications,
+				cart: req.session.cart,
+				wishlist: req.session.wishlist
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
+
+router.post("/store", function (req, res) {
+
+	db.Product.findAll({
+		where: { 
+			[Op.or]: [ {marca: req.body.search}, {nombre: req.body.search} ]}
+	})
+		.then((marca) => {
+			let resu = marca.map(item => {return item.dataValues.idProducto  })
+			console.log(resu)
+			//console.log(marca[0].dataValues.idProducto)
+			return db.Publication.findAll({
+				include: ["product"],
+				where: { idProducto: resu }
+			})
+		})
+	.then((publications) => {
+		res.render("store", {
+			session: req.session,
+			publications,
+			cart: req.session.cart,
+			wishlist: req.session.wishlist
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+		res.json({ error: true });
+	});
+})
 
 // Trayendo Estados
 router.get("/traer", function (req, res) {
-  db.Condition.findAll()
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Condition.findAll()
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo Usuarios
 router.get("/traerUsuarios", function (req, res) {
-  db.User.findAll({
-    include: ["Cart", "Facturacion", "Wishlist"],
-    raw: true,
-    nest: true,
-  })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.User.findAll({
+		include: ["Cart", "Facturacion", "Wishlist"],
+		raw: true,
+		nest: true,
+	})
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo Carrito
 router.get("/traerCart", function (req, res) {
-  db.Cart.findAll({
-    include: ["User", "Items"],
-  })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Cart.findAll({
+		include: ["User", "Items"],
+	})
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo Facturas
 router.get("/traerFacturas", function (req, res) {
-  db.Invoice.findAll()
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Invoice.findAll()
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo medioDePago
 router.get("/traerformasPago", function (req, res) {
-  db.PaymentMethod.findAll()
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.PaymentMethod.findAll()
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Trayendo medioDePago
 router.get("/traercategorias", function (req, res) {
-  db.Category.findAll()
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: true });
-    });
+	db.Category.findAll()
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.json({ error: true });
+		});
 });
 
 // Dashboard Prueba
 router.get("/prueba", function (req, res) {
-  res.render("pruebadashboard");
+	res.render("pruebadashboard");
 });
 
 module.exports = router;
